8b2450eb25117ccd05bbd1ab9e1458f1c6a368a4
Marcela Errazquin <marcela.errazquin@disney.com>
2018-12-18 12:38:53 -0500

    Full solution for Day 18 (1 & 2)

--- 18.js
+++ 18.js
@@ -1,11 +1,10 @@
 const fs = require('fs')
-var grid = fs.readFileSync('18demo.txt').toString().split('\n').map(row => row.split(''))
-var size = grid.length
-console.log(size)
-console.log(grid)
-//open ground (.), trees (|), or a lumberyard (#)
 
 /*
+. - open 
+| - trees
+# - lumberyard
+
 An open acre will become filled with trees if three or more adjacent acres contained trees. Otherwise, nothing happens.
 
 An acre filled with trees will become a lumberyard if three or more adjacent acres were lumberyards. Otherwise, nothing happens.
@@ -13,10 +12,11 @@ An acre filled with trees will become a lumberyard if three or more adjacent acr
 An acre containing a lumberyard will remain a lumberyard if it was adjacent to at least one other lumberyard and at least one acre containing trees. 
     Otherwise, it becomes open.
 */
+
 const types = {
-    '.': { label: 'open ground', render: '.', conversionReq: '|||', converTo: '|', sortOrder: 1 },
-    '|': { label: 'trees', render: '|', conversionReq: '###', convertTo: '#', sortOrder: 2 },
-    '#': { label: 'lumberyard', render: '#', conversionReq: '........', convertTo: '.', sortOrder: 3 }
+    '.': { render: '.', conversionReq: '|||', conversion: '|' },
+    '|': { render: '|', conversionReq: '###', conversion: '#' },
+    '#': { render: '#', conversionReq: '|#', conversion: '.', opposite: true }
 }
 const sortVals = {
     '.': 1,
@@ -35,7 +35,18 @@ class Acre {
     }
 }
 
-function getNeighbors(x, y) {
+var grid = fs.readFileSync('18.txt').toString().trim().split('\r\n').map(
+    (row, rowIndex) => {
+        return row.split('').map((acre, colIndex) => {
+            return new Acre(colIndex, rowIndex, acre)
+        })
+    }
+)
+var size = grid.length
+
+
+function getNeighbors(acre) {
+    var x = acre.x, y = acre.y
     var n, s, e, w, ne, se, nw, sw
     n = y > 0 ? grid[y - 1][x] : ''
     s = y < (size - 1) ? grid[y + 1][x] : ''
@@ -51,7 +62,69 @@ function getNeighbors(x, y) {
 function neighborSort(a, b) {
     return sortVals[a] - sortVals[b]
 }
-console.log('neighbors for', grid[0][2], getNeighbors(2, 0))
 
+function tick(oldGrid) {
+    var newGrid = []
+    for (var y = 0; y < size; y++) {
+        if (!newGrid[y]) newGrid[y] = []
+        for (x = 0; x < size; x++) {
+            var currAcre = oldGrid[y][x]
+            var isConverting = getNeighbors(currAcre).includes(currAcre.type.conversionReq)
+            if (currAcre.type.opposite) {
+                isConverting = !isConverting
+            }
 
+            var newType = isConverting ? currAcre.type.conversion : currAcre.toString()
+            newGrid[y][x] = new Acre(x, y, newType)
+        }
+    }
+    return newGrid
+}
 
+function render(grid, print = false) {
+    var str = ''
+    grid.forEach((row, rowIndex) => str += (rowIndex + '   ' + row.join('') + '\n'))
+    if (print) console.log(str)
+    return str
+}
+
+function part1Solution(grid) {
+    var printedGrid = render(grid).split('')
+
+    var wooded = printedGrid.filter(val => val === '|').length;
+    var lumberYards = printedGrid.filter(val => val === '#').length
+    return wooded * lumberYards
+}
+var grids = {}
+grids[render(grid)] = [-1]
+
+var target = 1000000000
+for (var i = 0; i < target; i++) {
+    var newGrid = tick(grid)
+    var key = render(newGrid)
+
+    if (!grids[key]) {
+        grids[key] = []
+    }
+
+    if (i === 9) {
+        console.log('p1', part1Solution(newGrid))
+    }
+
+    grids[key].push(i)
+
+    if (grids[key].length > 1) {
+        var repeat = Math.abs(grids[key].reduce((acc, val) => acc - val), 0)
+        var pattern = grids[key][0] + ((target - grids[key][0]) % repeat) - 1
+        for (var gridKey in grids) {
+            if (grids[gridKey].includes(pattern)) {
+                var wooded = gridKey.split('').filter(val => val === '|').length;
+                var lumberYards = gridKey.split('').filter(val => val === '#').length
+                console.log('p2', wooded * lumberYards)
+                target = i
+                break;
+            }
+        }
+    }
+    grid = newGrid
+}
