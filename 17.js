593048207dd14dc7034a4d209429997d725a4b2c
Marcela Errazquin <merrazquin@gmail.com>
2018-12-17 21:17:54 -0500

    Converting some wet squares to standing water

--- 17.js
+++ 17.js
@@ -7,7 +7,7 @@ var minX = null, maxX = null, minY = null, maxY = null
 
 const types = {
     SPRING: { name: 'spring', display: '+' },
-    SAND: { name: 'sand', display: '.' },
+    DRY_SAND: { name: 'drySand', display: '.' },
     CLAY: { name: 'clay', display: '#' },
     WET_SAND: { name: 'wetSand', display: '|' },
     STANDING_WATER: { name: 'standingWater', display: '~' }
@@ -21,7 +21,7 @@ class Square {
     }
 
     convertType(type) {
-        if(this.type == types.CLAY || this.type == types.STANDING_WATER) throw new Error(`Cannot convert ${this.type.name} to ${this.type.name}`)
+        if (this.type == types.CLAY || this.type == types.STANDING_WATER) throw new Error(`Cannot convert ${this.type.name} to ${this.type.name}`)
 
         this.type = type
     }
@@ -30,8 +30,44 @@ class Square {
         return this.type !== types.SPRING && this.type !== types.CLAY && this.type !== types.STANDING_WATER
     }
 
+    isConvertible() {
+        return this.type === types.DRY_SAND || this.type === types.WET_SAND
+    }
+
     toString() {
         return this.type.display
+    }
+
+    getNeighbor(direction) {
+        var neighbor
+        switch (direction) {
+            case 'south':
+                neighbor = this.southNeighbor()
+                break;
+            case 'north':
+                neighbor = this.northNeighbor()
+                break;
+            case 'east':
+                neighbor = this.eastNeighbor()
+                break;
+            case 'west':
+                neighbor = this.westNeighbor()
+                break;
+        }
+        return neighbor
+    }
+
+    westNeighbor() {
+        return grid[this.y][this.x - 1]
+    }
+    eastNeighbor() {
+        return grid[this.y][this.x + 1]
+    }
+    northNeighbor() {
+        return grid[this.y - 1][this.x]
+    }
+    southNeighbor() {
+        return grid[this.y + 1][this.x]
     }
 }
 
@@ -68,7 +104,7 @@ function generateGrid() {
         grid[0] = []
     }
     for (var x = minX; x <= maxX; x++) {
-        grid[0][x] = new Square(x, 0, (x === SPRING_COL ? types.SPRING : types.SAND))
+        grid[0][x] = new Square(x, 0, (x === SPRING_COL ? types.SPRING : types.DRY_SAND))
     }
 
     // fill in sand
@@ -78,7 +114,7 @@ function generateGrid() {
         }
         for (var x = minX; x <= maxX; x++) {
             if (!grid[y][x]) {
-                grid[y][x] = new Square(x, y, types.SAND)
+                grid[y][x] = new Square(x, y, types.DRY_SAND)
             }
         }
     }
@@ -95,30 +131,39 @@ function render() {
     return grid.reduce((rowAcc, row) => rowAcc + row.reduce((colAcc, square) => colAcc + square, '') + '\n', '')
 }
 
-function flowWater() {
-    var wetSquares = 0;
-    var currentSquare = grid[1][SPRING_COL]
-    console.log(currentSquare)
-    while (currentSquare && currentSquare.type != types.CLAY && currentSquare.type != types.STANDING_WATER) {
-        currentSquare.convertType(types.WET_SAND)
-        if (currentSquare.y > minY && currentSquare.y < maxY) {
-            wetSquares++
+function flowWater(direction, prevSquare) {
+    var currSquare = prevSquare.getNeighbor(direction)
+    while (currSquare && currSquare.isConvertible()) {
+        currSquare.convertType(types.WET_SAND)
+        var nextSquare = currSquare.getNeighbor(direction)
+        if(!nextSquare || !nextSquare.isAccessible()) {
+            currSquare.convertType(types.STANDING_WATER)
         }
-        currentSquare = findNextSquare(currentSquare)
+        prevSquare = currSquare
+        currSquare = nextSquare //currSquare.getNeighbor(direction)
     }
+    console.log(currSquare)
+    console.log(prevSquare)
+    if(direction === 'south') {
+        flowWater('east', prevSquare)
+        flowWater('west', prevSquare)
+    }
+    
 }
 
-function findNextSquare(curSquare) {
-    var southSquare = grid[curSquare.y + 1][curSquare.x]
-    var westSquare = grid[curSquare.y][curSquare.x - 1]
-    var eastSquare = grid[curSquare.y][curSquare.x + 1]
-    var northSquare = grid[curSquare.y - 1][curSquare.x]
+function findNextSquare(currSquare) {
+    var southSquare = grid[currSquare.y + 1][currSquare.x]
+    var westSquare = grid[currSquare.y][currSquare.x - 1]
+    var eastSquare = grid[currSquare.y][currSquare.x + 1]
+    var northSquare = grid[currSquare.y - 1][currSquare.x]
 
-    if(southSquare.isAccessible()) return southSquare;
-    if(westSquare.isAccessible()) return westSquare;
+    if (southSquare.isAccessible()) return southSquare;
+    if (westSquare.isAccessible()) return westSquare;
+    // if(eastSquare.isAccessible()) return eastSquare;
+    // if(northSquare.isAccessible()) return northSquare;
     return null
 }
 
 generateGrid()
-flowWater()
+flowWater('south', grid[0][SPRING_COL])
 console.log(render())
